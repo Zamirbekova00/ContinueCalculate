093b6c10f8b6fb323f28b0e63cd98edd545e262c
Zamirbekova00 <zamyrbekova@icloud.com>
2023-01-30 11:59:39 +0600

    continuation of homework

--- app/src/main/java/com/example/calculate/SecondActivity.java
+++ app/src/main/java/com/example/calculate/SecondActivity.java
@@ -2,13 +2,28 @@ package com.example.calculate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Log.d("koko", "onCreate: SecondActivity");
+
+        TextView textView = findViewById(R.id.g_text);
+        textView.setText(getIntent().getStringExtra("result"));
+        Button redButton = findViewById(R.id.red_button);
+        redButton.setOnClickListener(view -> {
+            finishAffinity();
+        });
     }
 }
